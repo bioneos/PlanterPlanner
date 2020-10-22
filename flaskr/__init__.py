f9d53f7c02012cb4f72655472e062398b62ae1b1
Joey Cicchese <jcicchese@Joeys-MBP.bioneos.com>
2020-10-22 13:49:42 -0500

    Added in reading the data.json file that contains our plant data. Edited the home/index.html to include a statment with a plant name. Added some comments.

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, current_app
+import json
 
 def create_app():
   # create the app
@@ -10,9 +11,18 @@ def create_app():
   def hello():
     return 'Hello, World!'
 
+  # app for the main homepage for the user
+  # to select the plants that they have
   @app.route('/', methods=['GET'])
   def home():
+    # currently manually defining the plants in our "database"
     plants = ['tomato', 'zucchini', 'rose']
-    return render_template('home/index.html', plants=plants)
+  
+    # Read in the data from the .json file
+    with current_app.open_resource("data.json", "r") as read_file:
+      data = json.load(read_file)
+
+    # return renders the html page at home/index.html
+    return render_template('home/index.html', plants=plants, data=data)
 
   return app
