c0e3d998ac4fc9a16ff334f2e61aa2e03f6cb3fb
Joey Cicchese <jcicchese@Joeys-MacBook-Pro.local>
2020-10-23 14:54:05 -0500

    Some code clean up and commenting for new routes to handle requests.

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -36,12 +36,14 @@ def create_app():
     # home page will return the plant_selection list supplied
     return render_template('home/index.html', plants=plants, data=plant_selection, sizes=spaces)
 
+  # route to return all plant names
   @app.route('/plants')
   def plants():
     # currently manually defining the plants from our "database"
     plants = {"plants" : ['tomato', 'zucchini', 'rose', 'lettuce', 'calla lily']}
     return plants
 
+  # route to return all plant data
   @app.route('/plantdata')
   def plantdata():
     # This location just returns the full dictionary that
@@ -50,6 +52,8 @@ def create_app():
      
     return data
 
+  # route the handle requests with the query for
+  # data on a single plant
   @app.route('/singleplant', methods=["GET"])
   def singleplant():
     # get the query information from the request
@@ -58,10 +62,13 @@ def create_app():
     data = readPlantData()
     plant_data = data['plant']
     
+    # find the plant information matching the query
     plant_dict = next(item for item in plant_data if item["name"] == plant_name)
 
     return plant_dict
 
+  # This function acts as our "fake database" call
+  # It reads in the plant data from the manually generated data.json file
   def readPlantData():
     with current_app.open_resource("data.json", "r") as read_file:
       data = json.load(read_file)
