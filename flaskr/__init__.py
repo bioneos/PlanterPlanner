1f4d55f788eac200ab10a198d649398aefd66aa1
Joey Cicchese <jcicchese@wireless-nat-guest-inside.uiowa.edu>
2020-10-26 16:25:50 -0500

    Removed homepage function and hello route that rendered the homepage.

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -5,37 +5,6 @@ def create_app():
   # create the app
   app = Flask(__name__)
 
-  # simple hello world page to get the
-  # app running
-  @app.route('/hello')
-  def hello():
-    return 'Hello, World!'
-
-  # app for the main homepage for the user
-  # to select the plants that they have
-  @app.route('/', methods=['GET', 'POST'])
-  def home():
-    # currently manually defining the plants in our "database"
-    plants = ['tomato', 'zucchini', 'rose', 'lettuce', 'calla lily']
-  
-    # Read in the data from the .json file
-    with current_app.open_resource("data.json", "r") as read_file:
-      data = json.load(read_file)
-
-    plant_data = data['plant']
-    # Get the plant section list
-    plant_selection = request.form.getlist('plant_select')
-
-    # loop through the plants to get the required space
-    spaces = []
-    for plant in plant_selection:
-      curr_dict = next(item for item in plant_data if item["name"] == plant)
-      spaces.append(curr_dict["space"])
-
-    # return renders the html page at home/index.html
-    # home page will return the plant_selection list supplied
-    return render_template('home/index.html', plants=plants, data=plant_selection, sizes=spaces)
-
   # route to return all plant names
   @app.route('/plants')
   def plants():
